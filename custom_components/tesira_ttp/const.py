ca24c72650680c2d38df8ff22c2cebbceee47b34
Darnel Kumar <darnel.kumar@dgk.one>
2026-03-26 00:31:50 +0000

    Set DEFAULT_PORT to 22
     - Change DEFAULT_PORT from 23 to 22 (switch to SSH port).

--- custom_components/tesira_ttp/const.py
+++ custom_components/tesira_ttp/const.py
@@ -1,8 +1,8 @@
 # #################################################################################################################### #
 # Filename: \custom_components\tesira_ttp\const.py                                                                     #
 # Repository: tesira_ttp                                                                                               #
-# Created Date: Thursday, March 19th 2026, 12:56:52 AM                                                                 #
-# Last Modified: Thursday, March 26th 2026, 12:26:20 AM                                                                #
+# Created Date: Sunday, March 22nd 2026, 10:04:37 PM                                                                   #
+# Last Modified: Thursday, March 26th 2026, 12:29:09 AM                                                                #
 # Original Author: Darnel Kumar                                                                                        #
 # Author Github: https://github.com/Darnel-K                                                                           #
 #                                                                                                                      #
@@ -42,7 +42,7 @@ CONF_MIN_DB = "min_db"
 CONF_MAX_DB = "max_db"
 CONF_STEP_DB = "step_db"
 
-DEFAULT_PORT = 23
+DEFAULT_PORT = 22
 DEFAULT_CONTROL_NAME = "Tesira Volume"
 DEFAULT_CHANNEL = 1
 DEFAULT_MIN_DB = -100.0
