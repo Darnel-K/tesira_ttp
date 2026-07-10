b8494a188cd9fe252f12595bd4a9d79c36bfce14
Darnel Kumar <darnel.kumar@dgk.one>
2026-07-10 23:45:55 +0100

    Add AGPL headers to Tesira client modules
    Add standardized file headers to `tesira_client/__init__.py`, `tesira_client/cli.py`, and `tesira_client/client.py` with author metadata, repository details, copyright, and GNU AGPL v3 licensing terms to ensure consistent attribution and licensing notices.

--- custom_components/tesira_ttp/tesira_client/client.py
+++ custom_components/tesira_ttp/tesira_client/client.py
@@ -1,3 +1,27 @@
+# #################################################################################################################### #
+# Filename: \custom_components\tesira_ttp\tesira_client\client.py                                                      #
+# Repository: tesira_ttp                                                                                               #
+# Created Date: Friday, July 10th 2026, 1:05:48 AM                                                                     #
+# Last Modified: Friday, July 10th 2026, 11:38:20 PM                                                                   #
+# Original Author: Darnel Kumar                                                                                        #
+# Author Github: https://github.com/Darnel-K                                                                           #
+#                                                                                                                      #
+# License: GNU Affero General Public License v3.0 only - https://www.gnu.org/licenses/agpl.txt                         #
+# Copyright (c) 2026 Darnel Kumar                                                                                      #
+#                                                                                                                      #
+# This program is free software: you can redistribute it and/or modify                                                 #
+# it under the terms of the GNU Affero General Public License as published                                             #
+# by the Free Software Foundation, either version 3 of the License, or                                                 #
+# (at your option) any later version.                                                                                  #
+#                                                                                                                      #
+# This program is distributed in the hope that it will be useful,                                                      #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                                                       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                        #
+# GNU Affero General Public License for more details.                                                                  #
+#                                                                                                                      #
+# You should have received a copy of the GNU Affero General Public License                                             #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.                                               #
+# #################################################################################################################### #
 from __future__ import annotations
 
 import asyncio
